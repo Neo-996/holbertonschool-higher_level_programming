057367f98d58f8b84d19e50b151e616f2d508ec2
root <abdulelahmalshehri@gmail.com>
2025-05-17 19:13:16 +0000

    hotfix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:56] + str[115:120] + str[56:67]
-print(str)
+print(str[39:56] + ' ' + str[115:120] + ' ' + str[:6])
