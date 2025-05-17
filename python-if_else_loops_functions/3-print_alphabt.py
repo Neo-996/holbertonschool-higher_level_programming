b2465c303629c1c973bd34241c65e8847159412e
root <abdulelahmalshehri@gmail.com>
2025-05-17 19:47:56 +0000

    hotfix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i != 101 and i != 113):
-        print('{:c}'.format(i), end='')
+[print(chr(c),end='') for c in range(97,123) if chr(c) not in ['q','e']]
