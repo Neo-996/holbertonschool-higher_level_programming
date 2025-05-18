079463366615039e40d0767a99cfbf4a08f6f255
root <abdulelahmalshehri@gmail.com>
2025-05-18 19:30:16 +0000

    hotfix

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-
     try:
-        val = int(value)
-        print("{:d}" .format(value))
+        print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
+    except (ValueError, TypeError):
         return False
