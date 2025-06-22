8abe114b85a36533d3afced38d5b99692045396d
root <abdulelahmalshehri@gmail.com>
2025-06-22 19:55:40 +0000

    hotfix

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""
-Module defining the Student class with selective JSON serialization.
-"""
+"""Student class with serialization support."""
+
 
 class Student:
+    """Defines a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Init attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieve dictionary representation of the Student instance.
+        """Return dict repr of instance.
 
-        If attrs is a list of strings, only attributes named in this list
-        that exist in the instance are included.
-        Otherwise, all attributes are returned.
+        If attrs is a list of strings, return only matching keys.
         """
-        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
-            # Filter only attributes in attrs and present in instance __dict__
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-        # Return all attributes
-        return self.__dict__.copy()
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            return {
+                k: getattr(self, k)
+                for k in attrs if hasattr(self, k)
+            }
+        return self.__dict__
