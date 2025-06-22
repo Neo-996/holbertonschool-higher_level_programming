179d78b2b5bc49fb4a85430adc763d3be9b4158f
root <abdulelahmalshehri@gmail.com>
2025-06-22 19:16:56 +0000

    hotfix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Module to write a string to a UTF-8 text file."""
 
+
 def write_file(filename="", text=""):
     """Write text to a file and return number of characters written."""
     with open(filename, "w", encoding="utf-8") as f:
+        # Write text and return number of characters written
          return f.write(text)
