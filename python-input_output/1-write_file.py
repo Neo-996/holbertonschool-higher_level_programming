9d4e40ed1c4c4017b3a9255c2d3f9c7c221eae87
root <abdulelahmalshehri@gmail.com>
2025-06-22 19:21:21 +0000

    hotfix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,7 @@ This module defines a function that writes text to a file
 and returns character count.
 """
 
+
 def write_file(filename="", text=""):
     """Write text to a file and return number of characters written."""
     with open(filename, "w", encoding="utf-8") as f:
