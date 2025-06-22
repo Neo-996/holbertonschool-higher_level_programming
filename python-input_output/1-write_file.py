049b4f6ef9cd8d302ad948513a531606e153da98
root <abdulelahmalshehri@gmail.com>
2025-06-22 19:09:21 +0000

    hotfix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
+"""This module provides a function to write a string to a UTF-8 text file."""
+
+write_file(filename="", text=""):
     """
     Writes a string to a text file (UTF8) and returns the number of characters written.
 
