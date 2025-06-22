f2d1f7a51689c1983d10691b7dd298a9ce66456a
root <abdulelahmalshehri@gmail.com>
2025-06-22 18:48:54 +0000

    hotfix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This module provides a function to read and print a UTF-8 text file to stdout."""
+
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints its content to stdout.
