1efb430617634dfa8d7c61f77750360a3a40d710
root <abdulelahmalshehri@gmail.com>
2025-06-22 19:35:15 +0000

    9th,10th tasks

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    (list, dict, string, int, bool) for JSON serialization of an object.
+    """
+    return obj.__dict__
---
+++ python-input_output/9-student.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Student:
+    """ Student class with first_name, last_name, and age attributes """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Return dictionary representation of the Student instance """
+        return self.__dict__
