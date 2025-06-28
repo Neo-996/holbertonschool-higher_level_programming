24c971a88d789a11cdf28f8c821ea69c76fcc33d
root <abdulelahmalshehri@gmail.com>
2025-06-28 23:19:05 +0000

    updated

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
--- Creates user_0d_1 with all privileges and password user_0d_1_pwd
--- Does not fail if the user already exists
-
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+-- Creates user `user_0d_1`, sets pwd, and grants all privileges.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
