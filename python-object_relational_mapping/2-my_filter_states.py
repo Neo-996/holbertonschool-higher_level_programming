8688612502a413b84d43742e79d14f8022a9dbef
root <abdulelahmalshehri@gmail.com>
2025-06-29 19:50:54 +0000

    updated

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Script that takes in arguments and displays all values in states table
-where name matches the argument (case-sensitive)."""
+"""Lists all states with a name matching the argument."""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    cursor = connection.cursor()
-    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(sys.argv[4])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
     cursor.execute(query)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
     cursor.close()
-    connection.close()
+    db.close()
