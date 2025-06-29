dcd847ae766e755d9ce7db9e18dc1e48a554f705
root <abdulelahmalshehri@gmail.com>
2025-06-29 19:25:55 +0000

    updated

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,29 +4,28 @@ Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # MySQL credentials and DB name from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=argv[1], passwd=argv[2],
+        db=argv[3], charset="utf8"
+    )
 
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=db_name)
-
-    # Create a cursor object
+    # Create a cursor
     cursor = db.cursor()
 
-    # Execute query to find states starting with 'N'
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Execute the query (may return extra results if collation is case-insensitive)
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Print all matching rows
-    for row in cursor.fetchall():
-        print(row)
+    # Filter in Python for strict case sensitivity
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
 
-    # Close cursor and database connection
+    # Cleanup
     cursor.close()
     db.close()
