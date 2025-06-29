3b4e7e57a3ead48eb61424650ce07f42942b423e
root <abdulelahmalshehri@gmail.com>
2025-06-29 20:16:47 +0000

    updated

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
 """
-This script lists all cities for a given state name from the
-hbtn_0e_4_usa database in MySQL, using a safe query.
+This script takes MySQL credentials and a state name as arguments,
+connects to the database, and prints all cities of the specified state,
+sorted by city id in ascending order.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
+    # Connect to the MySQL database using credentials from command line arguments
     db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
         user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        password=sys.argv[2],
+        database=sys.argv[3]
     )
-    cur = db.cursor()
-    cur.execute("""
+    # Create a cursor object to execute SQL queries
+    query = db.cursor()
+
+    # Execute a parameterized SQL query to get city names for the given state
+    query.execute("""
         SELECT cities.name
         FROM cities
-        JOIN states ON cities.state_id = states.id
+        INNER JOIN states
+        ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.id ASC
     """, (sys.argv[4],))
-    rows = cur.fetchall()
-    if rows:
-        print(", ".join([row[0] for row in rows]))
-    cur.close()
-    db.close()
+    
+    # Fetch all the matching rows
+    rows = query.fetchall()
+
+    # Extract city names from the rows
+    cities = [row[0] for row in rows]
+
+    # Print city names separated by commas
+    print(*cities, sep=", ")
