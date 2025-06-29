28b3b18ede11a38307380bb3330df2e4d4609410
root <abdulelahmalshehri@gmail.com>
2025-06-29 19:48:47 +0000

    updated

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,43 +1,23 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name matching the given argument
-from the database hbtn_0e_0_usa.
-
-Usage:
-    ./2-my_filter_states.py <mysql username> <mysql password> \
-<database name> <state name>
-"""
+"""Script that takes in arguments and displays all values in states table
+where name matches the argument (case-sensitive)."""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Connect to the MySQL database using command line arguments
     connection = MySQLdb.connect(
         host="localhost",
-        user=sys.argv[1],   # MySQL username
-        passwd=sys.argv[2], # MySQL password
-        db=sys.argv[3],     # Database name
-        port=3306           # Default MySQL port
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    # Create a cursor to execute SQL queries
     cursor = connection.cursor()
-
-    # Build the SQL query with exact case-sensitive filtering
-    query = ("SELECT * FROM states "
-             "WHERE BINARY name = '{}' "
-             "ORDER BY states.id ASC"
-             ).format(sys.argv[4])
-
-    # Execute the SQL query
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(sys.argv[4])
     cursor.execute(query)
-
-    # Fetch and print all resulting rows
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-
-    # Close the cursor and the database connection
     cursor.close()
     connection.close()
