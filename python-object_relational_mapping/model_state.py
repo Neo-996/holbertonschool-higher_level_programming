9cffac54d6ed465f613bc0e936937c8e57137b63
root <abdulelahmalshehri@gmail.com>
2025-06-30 15:01:13 +0000

    adjusted to pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,13 +4,13 @@ Defines the State class mapped to the 'states' table in a MySQL database.
 Also creates the Base instance for SQLAlchemy ORM mapping.
 """
 
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
+    
     """
     Represents the 'states' table.
     Each instance corresponds to a row in the table.
