4af246279fc8752d93b3866b118595fd6495657d
root <abdulelahmalshehri@gmail.com>
2025-06-30 15:28:21 +0000

    adjusted to pycodestyle

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,23 +12,30 @@ from model_city import City
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./14-model_city_fetch_by_state.py <username> <password> <database>")
+        print("Usage: ./14-model_city_fetch_by_state.py "
+              "<username> <password> <database>")
         sys.exit(1)
 
     # Connect to MySQL database
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
         ),
         pool_pre_ping=True
     )
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query all cities, join with states to get state name, order by city id ascending
-    cities = session.query(City, State).join(State, City.state_id == State.id).order_by(City.id).all()
+    # Query all cities, join with states to get state name,
+    # order by city id ascending
+    cities = session.query(City, State).join(
+        State,
+        City.state_id == State.id
+    ).order_by(City.id).all()
 
     for city, state in cities:
-        print(f"{state.name}: ({city.id}) {city.name}")
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
