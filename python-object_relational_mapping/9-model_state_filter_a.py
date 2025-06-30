b4fe21ff2b6d46e83216fd080f704146532a2ad7
root <abdulelahmalshehri@gmail.com>
2025-06-30 15:09:00 +0000

    adjusted to pycodestyle

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,14 +11,18 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Connect to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query states containing letter 'a'
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(
+        State.name.like('%a%')
+    ).order_by(State.id).all()
 
     # Print states in the required format
     for state in states:
