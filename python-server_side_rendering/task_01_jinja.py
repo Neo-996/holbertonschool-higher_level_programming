6b3f820fe47d111f026b1803c99878530eb85e15
root <abdulelahmalshehri@gmail.com>
2025-07-20 07:59:05 +0000

    2nd task

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home route renders index.html
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# About page route renders about.html
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Contact page route renders contact.html
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    # Run app on port 5000 with debug mode on
+    app.run(debug=True, port=5000)
