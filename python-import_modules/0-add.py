46f2ab962d1a286865d05daaf04d78e07d2a5328
Neo-996 <abdulelahmalshehri@gmail.com>
2025-10-11 15:29:10 -0700

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -7,7 +7,6 @@ from add_0 import add  # Importing add() function only once
 def main():
     a = 1  # Define a
     b = 2  # Define b
-    print(f"{a} + {b} = {add(a, b)}")  # Using one print statement with string format
-
-if __name__ == "__main__":
-    main()  # This ensures the code runs only when executed directly
+    if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    
