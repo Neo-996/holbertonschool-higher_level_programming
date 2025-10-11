39b8638e1a417eb2cbee4490f43a2963c7258858
Neo-996 <abdulelahmalshehri@gmail.com>
2025-10-11 15:29:48 -0700

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""Module to add two integers and print the result in a formatted string
-"""
 
-from add_0 import add  # Importing add() function only once
+from add_0 import add
 
-def main():
-    a = 1  # Define a
-    b = 2  # Define b
-    if __name__ == "__main__":
+a = 1
+b = 2
+if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
-    
