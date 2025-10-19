1ab34c7c0ec003dd675fc4742cb31b642731f862
Neo-996 <abdulelahmalshehri@gmail.com>
2025-10-19 13:20:28 -0700

    Fix docstring and error message in print_square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,16 +6,16 @@ This module defines a function that prints a square with '#' characters.
 
 def print_square(size):
     """
-    Prints a square with the character #.
+    Prints a square with '#' character.
 
     Args:
         size (int): The size length of the square.
 
     Raises:
-        TypeError: If size is not an integer or is a negative float.
+        TypeError: If size is not an integer.
         ValueError: If size is less than 0.
     """
-     if not isinstance(size, int):
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
